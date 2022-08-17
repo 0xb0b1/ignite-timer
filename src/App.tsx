6381ce210dd2d0f135aacaa34997e1fff51223a5
Paulo Vicente <vcente82@gmail.com>
2022-08-17 12:30:27 -0400

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
